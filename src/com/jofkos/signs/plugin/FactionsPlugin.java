5a0868ddb0f82954f64af5a027394da9279b3e31
tastybento <tastybento@wasteofplastic.com>
2017-01-01 21:21:49 -0800

    Mavenized.

--- src/com/jofkos/signs/plugin/FactionsPlugin.java
+++ src/com/jofkos/signs/plugin/FactionsPlugin.java
@@ -5,7 +5,6 @@ import org.bukkit.entity.Player;
 
 import com.jofkos.signs.utils.API;
 import com.massivecraft.factions.engine.EngineMain;
-import com.massivecraft.factions.listeners.FactionsListenerMain;
 import com.massivecraft.massivecore.ps.PS;
 
 public class FactionsPlugin extends API.APIPlugin {
@@ -13,24 +12,13 @@ public class FactionsPlugin extends API.APIPlugin {
 	static {
 		clazz = "com.massivecraft.factions.Factions";
 	}
-	
-	private boolean oldAPI = false;
-	
+		
 	public FactionsPlugin() {
-		try {
-			EngineMain.class.getName();
-		} catch (NoClassDefFoundError e) {
-			oldAPI = true;
-		}
 	}
 	
 	@Override
 	public boolean canBuild(Player player, Block block) {
-		if (oldAPI) {
-			return FactionsListenerMain.canPlayerBuildAt(player, PS.valueOf(block), true);			
-		} else {
 			return EngineMain.canPlayerBuildAt(player, PS.valueOf(block), true);
-		}
 	}
 
 }
